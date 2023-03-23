e54b4d29930ce7b65b409b27e058aa18f06259c2
okeseyi8 <okeseyi8@gmail.com>
2023-03-23 15:05:03 -0700

    happy

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
-* main - prints the largest of 3 integers
+* main - takes a date and prints how many days are left in the year, taking
+* leap years into account
 * Return: 0
 */
 
 int main(void)
 {
-        int a, b, c;
-        int largest;
+    int month;
+    int day;
+    int year;
 
-        a = 972;
-        b = -98;
-        c = 0;
+    month = 4;
+    day = 01;
+    year = 1997;
 
-        largest = largest_number(a, b, c);
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
 
-        printf("%d is the largest number\n", largest);
+    day = convert_day(month, day);
 
-        return (0);
+    print_remaining_days(month, day, year);
+
+    return (0);
 }
